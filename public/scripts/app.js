2f0c8d1f6e624b888f0f951c93067df308b1a38a
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-07-06 13:06:01 -0700

    Continued course

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,31 @@
 "use strict";
 
 console.log("app is running");
+
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ["one", "two"]
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Hello World"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle && app.options && app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? app.options : "N/A"
+    )
 );
 
 var appRoot = document.getElementById("app");
--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 console.log("app is running");
-var template = <h1>Hello World</h1>;
+
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ["one", "two"]
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{(app.subtitle && app.options) && app.subtitle}</p>
+        <p>{app.options.length > 0 ? app.options : "N/A"}</p>
+    </div>
+);
+
 
 var appRoot = document.getElementById("app");
 
