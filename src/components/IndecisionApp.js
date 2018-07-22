10f41c562120d8d231339f0e35554fb3c0ca2ac9
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-07-22 15:38:15 -0700

    Finished React Modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,12 +3,21 @@ import Header from "./Header";
 import Action from "./Action";
 import AddOption from "./AddOption";
 import Options from "./Options";
+import OptionModal from "./OptionModal";
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
 
+    handleClearSelectedOption = () => {
+        this.setState(() => ({
+            selectedOption: undefined
+        }))
+    }
+
+    
     handleDeleteOptions = () => {
         // this.setState(()=> {
         //     return {
@@ -28,7 +37,9 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({
+            selectedOption: option
+        }));
     }
 
     handleAddOption = (option) => {
@@ -82,6 +93,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption
                     handleAddOption={this.handleAddOption}
+                />
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         )
